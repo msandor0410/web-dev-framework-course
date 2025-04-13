ca7d23137d53d96cba2e63234deba5c421ba9571
msandor0410 <143738478+msandor0410@users.noreply.github.com>
2025-04-13 15:34:49 +0200

    Quick changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyAoH2abcDty0WYbE-JCLxQ-eS2JTJLqwgI",
+      authDomain: "survey-app-62d29.firebaseapp.com",
+      projectId: "survey-app-62d29",
+      storageBucket: "survey-app-62d29.firebasestorage.app",
+      messagingSenderId: "305467030963",
+      appId: "1:305467030963:web:4fe745497a76443a51dfc6",
+      measurementId: "G-HQES41CBEG"
+    }
+  };
+  
